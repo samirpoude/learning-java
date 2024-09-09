35e57707ea15a6bd735b5eae2efa5d4ce05fe4e8
samirpoudel <poudelsamir2016@gmail.com>
2024-09-09 14:30:20 +0930

    testing tutorial

---
+++ softwareTesting/tests/tempratureConverterTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class tempratureConverterTest {
+
+    @Test
+    void main() {
+
+    }
+
+    @Test
+    void celsiusToFarenheit() {
+
+    }
+
+    @Test
+    void farenheitToCelsius() {
+    }
+}
