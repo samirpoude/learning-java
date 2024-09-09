bda800a7146182e31d1e884fd83e2ac43f3a3367
samirpoudel <poudelsamir2016@gmail.com>
2024-09-09 21:49:00 +0930

    testing tutorial

--- softwareTesting/tests/tempratureConverterTest.java
+++ softwareTesting/tests/tempratureConverterTest.java
@@ -4,17 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class tempratureConverterTest {
 
-    @Test
-    void main() {
-
-    }
 
     @Test
     void celsiusToFarenheit() {
+        tempratureConverter m = new tempratureConverter();
+        assertEquals(32.0, m.celsiusToFarenheit(0));
+
 
     }
 
     @Test
     void farenheitToCelsius() {
+        tempratureConverter m = new tempratureConverter();
+        assertEquals(0.0, m.farenheitToCelsius(32.0));
     }
 }
