6267179c72b07b67eaedcae9aeaa9e837685f34c
samirpoudel <poudelsamir2016@gmail.com>
2024-09-14 11:10:26 +0930

    exercise 3

--- ConstructorExercise2/src/Book.java
+++ ConstructorExercise2/src/Book.java
@@ -1,2 +1,32 @@
 public class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Book(String title, String author, double price){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book(){
+        this("Unknown", "None", 0.0);
+    }
 }
--- ConstructorExercise2/src/Main.java
+++ ConstructorExercise2/src/Main.java
@@ -1,2 +1,31 @@
+/*
+3. Constructor Overloading
+Write a Java program to create a class called "Book" with instance variables title, author, and price.
+Implement a default constructor and two parameterized constructors:
+
+ a. One constructor takes title and author as parameters.
+  b. The other constructor takes title, author, and price as parameters.
+   c. Print the values of the variables for each constructor.
+ */
+
 public class Main {
+    public static void main(String[] args) {
+
+        Book book1 = new Book();
+        System.out.println("book1 title: " + book1.getTitle());
+        System.out.println("book1 author: " + book1.getAuthor());
+        System.out.println("book1 price: " + "$" +  book1.getPrice());
+
+        Book book2 = new Book(" thousand splendid sun ", " khaled housain ");
+        System.out.println("book2 title: " + book2.getTitle());
+        System.out.println("book2 author: " + book2.getAuthor());
+        System.out.println("book2 price: " + "$"+ book2.getPrice());
+
+        Book book3 = new Book(" how to not give a f*ck ", " mark manson ", 13.5);
+        System.out.println("book3 title: " + book3.getTitle());
+        System.out.println("book3 author: " + book3.getAuthor());
+        System.out.println("book3 price: " + "$" + book3.getPrice());
+
+
+    }
 }
